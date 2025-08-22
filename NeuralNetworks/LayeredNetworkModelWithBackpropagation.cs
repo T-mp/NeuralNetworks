0cf735520968353b9b2eafe71015a55d1298c0e9
Thomas P. <43186116+T-mp@users.noreply.github.com>
2025-08-22 22:51:02 +0200

    +LayeredNetworkModelWithBackpropagation

---
+++ NeuralNetworks/LayeredNetworkModelWithBackpropagation.cs
@@ -0,0 +1,24 @@
+﻿using Ivankarez.NeuralNetworks.Abstractions;
+using System;
+
+namespace Ivankarez.NeuralNetworks
+{
+    public class LayeredNetworkModelWithBackpropagation : LayeredNetworkModel
+    {
+        public LayeredNetworkModelWithBackpropagation(ISize inputs, params IModelLayerWithBackpropagation[] layers)
+            : base(inputs, layers)
+        {}
+
+        public float[] Backward(float[] outputError, float learningRate)
+        {
+            if (Layers.Count == 0) throw new InvalidOperationException("No layers to backpropagate through");
+            var layerErrors = outputError;
+            for (int i = Layers.Count - 1; i >= 0; i--)
+            {
+                var layer = (IModelLayerWithBackpropagation)Layers[i];
+                layerErrors = layer.Backward(layerErrors, learningRate);
+            }
+            return layerErrors;
+        }
+    }
+}
